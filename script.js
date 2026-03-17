cf83d0637f7c45763ac10d23fabadb27c6175503
Felipe6677 <felipeprates@colegiopaulodetarso.com.br>
2026-03-17 14:48:09 +0000

    estrutura HTML

---
+++ script.js
@@ -0,0 +1,8 @@
+const taskbtn = document.querySelector("taskbtn")
+const tasklist = document.querySelector("tasklist")
+const taskinput = document.querySelector("taskinput")
+
+taskbtn.addEventListener("click", function(){
+    alert("CLIQUEI !!!")
+})
+
